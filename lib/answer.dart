b607e4c39a84a132d3cc649de9e4c25d040a0905
YousifAladower <63420481+YousifAladower@users.noreply.github.com>
2023-09-08 18:43:55 +0400

    question1

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Asnwer extends StatelessWidget {
+  final String answerText;
+   final void Function() press;
+
+  Asnwer(this.press,this.answerText);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton( 
+             child: Text(answerText,
+             style: TextStyle(fontSize: 30),
+             ),
+             onPressed: press,
+             ),
+    );
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quize1/answer.dart';
 import 'package:quize1/question.dart';
+
 
 void main(){
    return runApp(MyApp());
@@ -11,10 +13,29 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-final _questions = [
-  'what \'s favorite color ?',
-  'what \'s favorite anamels ?',
-  'what \'s favorite place ?'
+int _questionsIdix=0;
+void answerQuestion()
+{
+  if(_questionsIdix==1)
+  _questionsIdix=-1;
+  setState(() {
+    _questionsIdix++;
+  });
+  print(_questionsIdix);
+  print("Yousif");
+}
+
+final List<Map<String, Object>> _questions = [
+
+  {
+    "questionText":"What is the capital of Iran?", 
+     "answers": ["Tehran","Mashhad", "<NAME>", "Kkdsjf"]
+  },
+  {
+    "questionText":"what 's favorite anamels ?",  
+    "answers": ["YOUS","AD", "ADF", "ASDFD"]
+  },
+
 ];
   @override
   Widget build(BuildContext context) {
@@ -26,7 +47,15 @@ final _questions = [
         body: Container(
           child: Column(
             children: [
-             Questions(_questions[0])
+             Questions(_questions[_questionsIdix]['questionText'] as String),
+              ...(_questions[_questionsIdix]['answers'] as List<String> ).map((answer)
+              {
+                return  Asnwer(answerQuestion,answer);
+
+              }).toList(),
+            
+             Asnwer(answerQuestion,"answer2"),
+             Asnwer(answerQuestion,"answer3"),
             ],
           ),
          // child:Questions(_questions[1])
